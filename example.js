e90038f358ceb27184263333ac270ec300f430dd
iPlayer <84263827+iiplayer@users.noreply.github.com>
2022-01-09 15:07:46 +0800

    Create example.js

---
+++ example.js
@@ -0,0 +1,36 @@
+'use strict'
+
+async function iPlayerMain(number) {
+    if (number == 1) {
+        getPage1()
+    }
+}
+
+async function getPage1() {
+    let param = {name: "phpfanjiashequ"};
+    let options = {
+        url : "http://api.hclyz.com:81/mf/jiekou.php",
+        timeout : 16
+    }
+    $httpClient.get(options, function(err, res, body){
+        let data = {
+            title: '平台列表',
+            canPlay: false,
+            config:{       
+                key: {
+                    name: 'name',
+                    plat: 'name',
+                    image:'img',
+                    address: 'dz',
+                    online: 'sl',
+                    time: '',
+                    totalTime: ''
+                }
+
+            }
+            data: body.data
+        }
+
+        $UI.reloadData(data)
+    })
+}
