d366ce40ad38ef3314c252ab0fcd0a8d671bfb59
iPlayer <84263827+iiplayer@users.noreply.github.com>
2022-07-19 15:25:15 +0800

    Update example.js

--- example.js
+++ example.js
@@ -1,75 +1,71 @@
+
+//example.js
+//iPlayer
+
 'use strict'
 
-let page1Data = []
-
-//UI入口方法
-//number： 页面编号从1开始；
-//index：点击当前页面的cell索引（从0开始）
-async function iPlayerMain(number, index) {
-    if (number == 1) {
-        getPage1()
-    }else if (number == 2) {
-        getPage2(index)
+async function iPlayerMain(number, index, page) {
+    var times = new Date();
+    let data = {
+        title: "EXAMPLE", //页面导航栏标题
+        canPlay: true, //若为true，则跳转播放器播放
+        data: [{
+            name: "example",
+            plat: "m3u8",
+            address: "http://stream1.jlntv.cn/xcpd/sd/live.m3u8",
+            time: times.toLocaleDateString()
+        }]
     }
+    console.log(`numben(页面)：${number}; index(选择项)：${index}; page(页码)：${page}`)
+    console.log(JSON.stringify(data))
+    iUI.reloadData(data)//刷新页面
 }
 
-async function getPage1() {
-    //请求参数
-    let options = {
-        url : "http://api.hclyz.com:81/mf/jiekou.php",
-        timeout : 16
-    }
-    //get请求
-    iNetwork.get(options, function(err, res, body){
-        page1Data = body.data
-        let data = {
-            title: '平台列表',//导航栏标题
-            canPlay: false,//当前数据是有可以播放（若为true则跳转至播放器播放“address”的地址）
-            config:{      //格式化数据使iplayer可以识别
-                key: {
-                    name: 'name',
-                    plat: 'name',
-                    image:'img',
-                    address: 'dz',
-                    online: 'sl',
-                    time: '',
-                    totalTime: ''
-                }
+/*
+ 
+ async function getData() {
 
-            },
-            data: body.data
-        }
-        //获取数据后刷新UI（data["data"]的值须为数组）
-        iUI.reloadData(data)
-    })
-}
+     let param = {key: 'value'}
+     let options = {
+         url : "http://example.php",
+         body : param, //可选值
+         timeout : 16, //可选值，默认16s
+         useJSON : false //可选值，默认false。若设置为true，则将body转为json data
+     }
+     iNetwork.get(options, function(err, res, body){
+         console.log(JSON.stringify(body))
+     })
+ }
+ 
+ async function postData() {
 
-async function getPage2(index) {
-    
-    let subData = page1Data[index]
-    let param = {name: subData['dz']};
-    //请求参数
-    let options = {
-        url : "http://api.hclyz.com:81/mf/jiekou.php",
-        body : param,
-        timeout : 16
-    }
-    //get请求
-    iNetwork.get(options, function(err, res, body){
-        let data = {
-            title: subData['name'],
-            canPlay: true,
-            config:{       
-                key: {
-                    name: 'title',
-                    image:'img',
-                    address: 'address'
-                }
-
-            },
-            data: body.data
-        }
-
-        iUI.reloadData(data)
-    })
-}
+     let param = {key: 'value'}
+     let options = {
+         url : "http://example.php",
+         body : param, //可选值
+         timeout : 16, //可选值，默认16s
+         useJSON : false //可选值，默认false。若设置为true，则将body转为json data
+     }
+     iNetwork.post(options, function(err, res, body){
+         console.log(JSON.stringify(body))
+     })
+ }
+ //数据存取
+ iData.write("value", "key")
+ iData.read("key")
+ 
+ //本地推送
+ iNotify.notify("Title", "subtitle", "detail", {"open-url": 'https://www.baidu.com', "media-url":'https://s3.bmp.ovh/imgs/2022/06/03/b00eeb1ee998105e.png'})
+ 
+ //显示HUD
+ iUI.showHUD('wait', '等待样式')
+ iUI.showHUD('succ', '成功样式')
+ iUI.showHUD('error', '失败样式')
+ iUI.showHUD('info', '显示信息')
+ //清除HUD
+ iUI.clearAllHUD()
+ 
+ //版本号
+ iUI.appVersion()
+ 
+ */
