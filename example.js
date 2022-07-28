4f35d0a8d3c9aa0e6be7f299a9b4cec138c11dde
iPlayer <84263827+iiplayer@users.noreply.github.com>
2022-07-28 11:54:12 +0800

    Update example.js

--- example.js
+++ example.js
@@ -5,6 +5,10 @@
 
 'use script'
 
+//UI入口方法：数据展示
+//number：页面层级从1开始；
+//index：点击的cell索引从0开始；
+//page：数据接口页数(从0开始)
 async function iPlayerMain(number, index, page) {
     try {
         //App版本号
@@ -13,20 +17,30 @@ async function iPlayerMain(number, index, page) {
         //等待样式HUD
         iUI.showHUD('wait', '加载中...')
         
-        await wait(3000) //等待3秒
+        await wait(2000) //等待2秒
         
         let times = new Date()
         let data = {
             //页面导航栏标题
             title: "EXAMPLE",
-            //若为true，则跳转播放器播放
+            //当前页面数据是否支持播放
             canPlay: true,
+            //是否显示多任务按钮
+            mutableDuty: false,
             //列表数据
             data: [{
                 name: "example",
                 plat: "m3u8",
                 address: "http://stream1.jlntv.cn/xcpd/sd/live.m3u8",
-                time: times.toLocaleDateString()
+                time: times.toLocaleDateString(),
+            },
+            {
+                name: "alert example",
+                plat: "alert",
+                address: "",
+                time: times.toLocaleDateString(),
+                //是否跳转至下一页（如果address有效则忽略）
+                pushNext: false
             }]
         }
         
@@ -37,9 +51,22 @@ async function iPlayerMain(number, index, page) {
         iUI.reloadData(data)//刷新页面
         
         iNotify.notify("iPlayer", "获取数据成功", "▶点击播放", {"open-url": data.data[0].address, "media-url":'https://s3.bmp.ovh/imgs/2022/06/03/b00eeb1ee998105e.png'})
+                
     } catch(err) {
         console.log(err)
     }
+}
+
+//cell点击事件（版本>=1.5.3）
+async function iPlayerDidSelectIndexPath(arg) {
+    //iUI.showHUD('info', `点击了第${arg.number}个页面的\n第${arg.index}个cell`)
+    if(arg.index == 0) return
+    iUI.alert('Title', `message`, ['取消:cancel', '红色:destructive', '输入框1:textField', '输入框2:textField', '普通', '播放'],(index, title, textFileds)=>{
+        console.log(index, title, textFileds)
+        if (title == '播放') {
+            iUI.play('http://stream1.jlntv.cn/xcpd/sd/live.m3u8')
+        }
+    })
 }
 
 async function wait(t) {
@@ -96,8 +123,19 @@ function version() {
  //清除HUD
  iUI.clearAllHUD()
  
+ //跳转至播放器（版本>=1.5.3可用）
+ iUI.play('rtmp://xxx')
+ 
  //获取版本号（版本>=1.5.0可用）
  iUI.appVersion()
+ 
+ //数据持久化
+ //读取key对应的数据
+ iUI.read('key')
+ //删除key对应的数据
+ iUI.remove('key')
+ //写入数据key相同会覆盖
+ iUI.write('value', 'key')
  
  */
 
